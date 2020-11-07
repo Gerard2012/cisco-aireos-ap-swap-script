4444b3cfab1b515d6d16d6f85e7bcf73fecf100e
Gerard2012 <59774244+Gerard2012@users.noreply.github.com>
2020-11-07 16:46:32 +0000

    Re-arranged some functions and added doc strings.

--- ap_replacement.py
+++ ap_replacement.py
@@ -1,4 +1,6 @@
-#############################################################################
+##############################################################################################
+# Global Variables & Config
+##############################################################################################
 
 #floor = input('Floor >> ')
 cdp_before = input('Pre swap CDP capture >> ')
@@ -18,25 +20,18 @@ old_ap_cdp = []
 old_ap_cdp_dict = {}
 ap_dict = {}
 
-#############################################################################
 
-def new_ap():
-
-	with open(cdp_after, 'r+') as f:
-		lines = f.readlines()
-		for line in lines:
-			if line.startswith('AP'):
-				line = line.split()
-				new_ap_cdp.append(line)
-
-	for elem in new_ap_cdp:
-		del elem[1]
-		elem[1:] = [' '.join(elem[1:])]
-		new_ap_cdp_dict[elem[0]] = elem[1]
-
-#############################################################################
+##############################################################################################
+# Functions
+##############################################################################################
 
 def old_ap():
+
+	"""
+	Takes the output of 'show ap cdp neighbors all' before the APs have been swapped
+	and parses this into 'AP: switch/port' items and appends to old_ap_cdp_dict{}.
+
+	"""
 
 	with open(cdp_before, 'r+') as f:
 		lines = f.readlines()
@@ -50,9 +45,42 @@ def old_ap():
 		elem[1:] = [' '.join(elem[1:])]
 		old_ap_cdp_dict[elem[0]] = elem[1]
 
-#############################################################################
+
+##############################################################################################
+
+def new_ap():
+
+	"""
+	Takes the output of 'show ap cdp neighbors all' after the APs have been swapped,
+	filters out new APs registered with default AP name (ie AP+MAC or AP0000.0000.0000)
+	and parses this into 'AP: switch/port' items and appends to new_ap_cdp_dict{}.
+
+	"""
+
+	with open(cdp_after, 'r+') as f:
+		lines = f.readlines()
+		for line in lines:
+			if line.startswith('AP'):
+				line = line.split()
+				new_ap_cdp.append(line)
+
+	for elem in new_ap_cdp:
+		del elem[1]
+		elem[1:] = [' '.join(elem[1:])]
+		new_ap_cdp_dict[elem[0]] = elem[1]
+
+
+##############################################################################################
 
 def sorted_ap():
+
+	"""
+	Compares values in new_ap_cdp_dict{} with vlaues in old_ap_cdp_dict{}.
+	If a match is found then the old_ap_cdp_dict{} key (the AP name) is combined with the 
+	new_ap_cdp_dict{} value (CDP switch/port info) and this new key/value pair is appended to 
+	ap_dict{}.
+
+	"""
 
 	n = start_num
 
@@ -61,11 +89,15 @@ def sorted_ap():
 			if new_ap_cdp_dict[i] == old_ap_cdp_dict[i2]:
 				ap_dict[i] = i2
 
+	## If no match is found (ie the AP is not patched to the same switch/port as any AP in the pre-swap CDP capture)
+	## then the AP is assigned a placeholder AP name. 
+	## This placeholder name can be specified when the script is run.
 	for i in new_ap_cdp_dict:
 		if new_ap_cdp_dict[i] not in old_ap_cdp_dict.values():
 			ap_dict[i] = f'NY-1221-AP-X' + str(n)
 			n += 1
 
+	## Outputs details of match to terminal.
 	print('\n')
 	print('Number of APs = ', len(ap_dict))
 	print('\n')
@@ -77,9 +109,15 @@ def sorted_ap():
 
 	print('\n')
 
-#############################################################################
+
+##############################################################################################
 
 def crt_commands():
+
+	"""
+	Writes the AireOS CLI commands to the SecureCRT .vbs file to reconfigure the new APs.
+
+	"""
 
 	for i in ap_dict:
 		new_ap_name = i
@@ -100,14 +138,22 @@ def crt_commands():
 \tcrt.Screen.WaitForString "er) >"\n
 		""")
 
-#############################################################################
+
+##############################################################################################
 
 def main():
+
+	"""
+	Runs the above functions consecutively.
+
+	"""
 
 	new_ap()
 	old_ap()
 	sorted_ap()
 
+	## Creates the SecureCRT .vbs file and writes the headers/footers to the file
+	## while also calling crt_commands() to write the CLI commands to the file.
 	with open(file, 'w+') as f:
 		f.write("""#$language = \"VBScript\"
 #$interface = \"1.0\"
@@ -124,6 +170,11 @@ Sub Main
 End Sub
 """)
 
-#############################################################################
 
-main()
+##############################################################################################
+# Run
+##############################################################################################
+
+if __name__ == '__main__':
+
+	main()
